b8d01201cedefe2a5fe55b60f40d752d36ffeae4
vuhongquan03 <vuhongquan2203@gmail.com>
2019-07-14 17:58:00 +0700

    first

---
+++ src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { UserComponent } from './user/user.component';
+import { CustomMaterialModule } from './core/material.module';
+import { FormsModule } from '@angular/forms';
+import {
+
+  MatButtonModule, MatCardModule, MatDialogModule, MatInputModule, MatTableModule,
+
+  MatToolbarModule, MatMenuModule, MatIconModule, MatProgressSpinnerModule
+
+} from '@angular/material';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    UserComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    CustomMaterialModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatButtonModule, MatCardModule, MatDialogModule, MatInputModule, MatTableModule,
+
+    MatToolbarModule, MatMenuModule, MatIconModule, MatProgressSpinnerModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
